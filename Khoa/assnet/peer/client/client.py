0a3f0154cac838dbd448fd4d20dbf36061207cdc
MeowStat <tinnie2004@gmail.com>
2024-12-10 08:01:00 +0700

    fix:unpack and pack successfully

--- Khoa/assnet/peer/client/client.py
+++ Khoa/assnet/peer/client/client.py
@@ -45,12 +45,22 @@ def request_piece_from_peer(address, piece_index, info_hash):
             print(f"size_header:{size_header}")
 
             piece_size = struct.unpack(">Q", size_header)[0]
-
-            data = conn.recv(piece_size)
-            print(data)
+            # print(piece_size)
+            data = receive_exactly(conn,piece_size)
+            # print(len(data))
             return data, None
     except Exception as e:
         return None, str(e)
+
+def receive_exactly(conn, size):
+    data = b""
+    while len(data) < size:
+        chunk = conn.recv(size - len(data))
+        if not chunk:
+            raise Exception("Connection closed prematurely")
+        data += chunk
+    return data
+
 
 def test_connection(address):
     try:
